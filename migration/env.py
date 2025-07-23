2f008ad4c77615c5634f403840681f1b1d60b480
Vlad <nocepro20122@gmail.com>
2025-07-23 15:12:53 +0300

    FS-004: auth in sqlite

--- migration/env.py
+++ migration/env.py
@@ -1,37 +1,28 @@
-
 import asyncio
 from logging.config import fileConfig
 
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+
 from alembic import context
 
+from src.database import Base
 from src.games.models import Game
 from src.providers.models import Provider
 from src.user.models import UserModel
-from src.declarative_base import Base
 from src.config import settings
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
 
 target_metadata = Base.metadata
-# target_metadata = None
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
 def run_migrations_offline() -> None:
